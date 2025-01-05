28a3a8423d576a28e56b7e9d53394db77d719e4e
INSIDERS <31087501+airdropinsiders@users.noreply.github.com>
2025-01-05 15:37:24 +0700

    v1.0

---
+++ accounts/accounts_tmp.js
@@ -0,0 +1,12 @@
+/**
+ * Accounts list file
+ * write your accounts like this
+ * export const accountLists = [
+ *     "MANGOPRIVKEY1",
+ *     "MANGOPRIVKEY2",
+ *     "MANGOPRIVKEY3",
+ * ];
+ *
+ */
+
+export const accountList = [];
